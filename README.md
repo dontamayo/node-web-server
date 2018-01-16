21feebc65a7942a56f4019ef18587cdc7fb69a24
Don aldrich Tamayo <amazingmardon@gmail.com>
2018-01-15 23:49:15 -0500

    adding express middleware, server logss

--- README.md
+++ README.md
@@ -5,5 +5,9 @@
 3. Rendering Templates with Data
 * using express
 * using Handlebars
-5. Advanced Templating
+4. Advanced Templating
 * nodemon server.js -e js,hbs
+5. Express middleware
+* logs on the screen
+* keep tracks of how our servers are working
+6.
--- server.js
+++ server.js
@@ -3,10 +3,29 @@ console.log('express app');
 const express = require('express');
 const hbs = require('hbs');
 const PORT = 3000;
+const fs = require('fs');
 let app = express();
 
 hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
+
+
+app.use((req, res, next) => {
+  let now = new Date().toString();
+  let log = (`${now}: ${req.method} ${req.url}`);
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to server.log.');
+    }
+  });
+  next();
+});
+
+// ======for maintenance purposes===========//
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
 //middleware
 app.use(express.static(__dirname + '/public'));
 
